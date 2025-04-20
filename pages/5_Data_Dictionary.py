0ca0c206863df2567894d17567939941d8366298
aggieed97 <aggieed97@gmail.com>
2025-04-20 15:29:17 -0500

    Centered title in Data Dictionary

--- pages/5_Data_Dictionary.py
+++ pages/5_Data_Dictionary.py
@@ -3,7 +3,6 @@ import pandas as pd
 
 # Data Dictionary for Retail Sales Dashboard
 # Set the title of the page
-st.title("Data Dictionary")
 st.markdown("<h1 style='text-align: center;'>Data Dictionary</h1>", unsafe_allow_html=True)
 
 # Add a description for the page
