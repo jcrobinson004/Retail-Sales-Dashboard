2c0a6880c98188e3d95e4dea53ab9cea409363d5
aggieed97 <aggieed97@gmail.com>
2025-04-19 22:30:31 -0500

    Add image to front page

--- Retail_Sales_Analysis.py
+++ Retail_Sales_Analysis.py
@@ -12,6 +12,17 @@ st.markdown("<h1 style='text-align: center;'>ADTA 5410 - Group 1 Weekly Challeng
 st.markdown("<h3 style='text-align: center;'>Jordan Robinson, Kelly Welander, Ed Salinas</h3>", unsafe_allow_html=True)
 st.markdown("<h4 style='text-align: center;'>Welcome to the Retail Analysis Page for Office Supplies</h4>", unsafe_allow_html=True)
 
-office_supplies_one = Image.open('./images/office-supplies-1.jpg')
+try:
+    office_supplies_one = Image.open('./images/office-supplies-1.png')
+    col1, col2, col3 = st.columns([1, 2, 1])
+
+    with col2:
+        st.image(office_supplies_one, caption='Courtesy: Platinum Copier Solutions')
+
+
+except FileNotFoundError:
+    st.error("Error: One or both image files were not found. Please check the paths.")
+except Exception as e:
+    st.error(f"An error occurred while loading the images: {e}")
 
 st.sidebar.success("Select a page from the navigation above.")
