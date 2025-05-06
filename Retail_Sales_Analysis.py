9e0b16542b17a18f8177581d7b5f27c5924960b2
aggieed97 <aggieed97@gmail.com>
2025-05-05 23:13:55 -0500

    Changed Main Title of First Page

--- Retail_Sales_Analysis.py
+++ Retail_Sales_Analysis.py
@@ -6,7 +6,7 @@ st.set_page_config(
     page_icon="🏠",
     layout="wide")
 
-st.markdown("<h1 style='text-align: center;'>ADTA 5410 - Group 1 Weekly Challenge 4 - Retail Sales Analysis</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center;'>ADTA 5410 - Group 1 Final Project - Retail Sales Analysis</h1>", unsafe_allow_html=True)
 
 # Center the subheader using Markdown and HTML
 st.markdown("<h3 style='text-align: center;'>Jordan Robinson, Kelly Welander, Ed Salinas</h3>", unsafe_allow_html=True)
